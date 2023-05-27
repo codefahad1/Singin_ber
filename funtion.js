660aa54ce7396d260011173bcc80df40db4fb3c1
MD Fahad Jamil <119007385+codefahad1@users.noreply.github.com>
2023-05-28 00:49:30 +0600

    Add files via upload

---
+++ funtion.js
@@ -0,0 +1,11 @@
+
+// JavaScript code to handle form submission
+document.querySelector('.signup-form').addEventListener('submit'), function(event) {
+event.preventDefault(); // Prevent form from submitting
+}
+// Fetch form values
+var username = document.querySelector('input[type="text"]').value;
+var email = document.querySelector('input[type="email"]').value;
+var password = document.querySelector('input[type="password"]').value;
+
+
